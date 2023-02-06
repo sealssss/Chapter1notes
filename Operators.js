fcae952f071b3900c28ee0f4ec9c03c196de4d3b
iuf7 <iuf7@fresnou.org>
2023-02-06 13:56:40 -0800

    initial commit

---
+++ Operators.js
@@ -0,0 +1,20 @@
+// Unary Operators are one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+
+//Binary Numbers use two values
+//Comparison Operators
+
+//Logical Operators
+console.log(10 > 5);  //greater than
+console.log(10 < 5); //less than
+console.log(10 >= 5); //greater than or equal to
+console.log(10 <= 5); //less than or equal to
+console.log(10 == 5); //are they both equal? false
+console.log(10 == 10); //are they both equal? true
+console.log(10 != 5); //are they different? true
+console.log(10 != 10); //are they different? false
+// Logical Operators
+
