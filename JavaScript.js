95234daad7c1259f7a4c8a7e2e8b6317b5f061ab
iuf7 <iuf7@fresnou.org>
2023-02-06 13:56:13 -0800

    initial commit

---
+++ JavaScript.js
@@ -0,0 +1,30 @@
+console.log(7);//This is a comment
+//Numbers//Integers
+console.log(-3);
+console.log(9);
+console.log(0);
+
+//Floating Point Numbers(Floats)
+console.log(3.14);
+console.log(-8.123456);
+
+//Strings//Characters, letters, symbols, spaces
+console.log("Life");
+console.log('!@#$ ps also a string');
+console.log(`Backticks by the 1`);
+
+//Arithmetic Operators (Math)console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives
+*console.log(14/2);
+console.log(14%2);
+//% is called MOD
+//it means reminder after dividing
+
+// Concatenation means to combine strings into a single string
+
+console.log("My name "+"is Ms. Bolin.");
+
+console.log("My"+ " "+ "age"+ " "+ "is "+ 14);
+
+console.log("Your age is\n16");
